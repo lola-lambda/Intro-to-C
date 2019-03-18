3c7b9255627b8ffc0ad08c7205573634c570c372
Lola <lolahef@gmail.com>
2019-03-18 18:51:20 -0400

    Strings not passing tests, but not sure why (outputs look fine)

--- strings/strings.c
+++ strings/strings.c
@@ -40,12 +40,13 @@ int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
-
+    char str1[] = "hello world";
     char rv[512];
+    char new[512];
 
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
     printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
-
+    printf("The string 'hello world' reversed is: '%s'\n", reverse_string(new, str1));
     return 0;
 }
 #endif
